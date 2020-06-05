5c23625c4435a5ec135de5674ec8c48fe55bba61
root <994481905@qq.com>
2020-06-05 19:22:31 +0800

    1

---
+++ leetcode/editor/cn/ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * @description:leetcodeExercise
+ * @author: winsonxu
+ * @createTime:2020/5/20
+ */
+
+public class ListNode {
+    int val;
+    // 下一个链表对象
+    ListNode next;
+
+    //赋值链表的值
+    ListNode(int x) {
+        val = x;
+    }
+}
